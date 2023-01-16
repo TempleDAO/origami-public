e7560791bed0fe999826e592883985f9e925f2a1
jebsley3 <cuiett@protonmail.com>
2023-01-16 19:40:27 +0000

    style: conditional logo size

--- apps/dapp/src/components/Header/Logo.tsx
+++ apps/dapp/src/components/Header/Logo.tsx
@@ -1,5 +1,16 @@
 import { Image } from '@/components/commons/Image';
+import { useMediaQuery } from '@/hooks/use-media-query';
+import { theme } from '@/styles/theme';
 
-export const Logo = () => (
-  <Image src="/header-logo.svg" alt="A logo for Origami" height={60} width={280}/>
-);
+export const Logo = () => {
+  const isDesktop = useMediaQuery(theme.responsiveBreakpoints.md);
+
+  return (
+    <Image
+      src="/header-logo.svg"
+      alt="A logo for Origami"
+      height={isDesktop ? 60 : 50}
+      width={isDesktop ? 280 : 234}
+    />
+  );
+};
