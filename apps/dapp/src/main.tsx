a1b4662a04b719edc049060235f3ddace57d6e29
Nicho <nichosystem@protonmail.com>
2023-07-24 22:43:05 -0500

    feat: reroute geoblocked users (#255)
    * feat: reroute geoblocked users
    
    * fix: update default geoblock state

--- apps/dapp/src/main.tsx
+++ apps/dapp/src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Buffer } from 'buffer';
-import { ThemeContext } from 'styled-components';
+import styled, { ThemeContext } from 'styled-components';
 
 import { GlobalStyle } from './styles/GlobalStyle';
 import { theme } from './styles/theme';
@@ -20,6 +20,7 @@ import { AppLayout } from './components/Layouts/AppLayout';
 import { AppRoutes } from './app-routes';
 import { Web3OnboardProvider } from '@web3-onboard/react';
 import { WEB3_ONBOARD } from './config/web3onboard';
+import { GEOBLOCK_URL } from './urls';
 
 // polyfill Buffer required for WalletConnect
 if (!window.Buffer) {
@@ -75,17 +76,56 @@ const router = createBrowserRouter([
 
 const API_CONFIG = getApiConfig();
 
+const AccessRestricted = () => (
+  <FullScreenContainer>
+    <h1>Access Restricted</h1>
+    <p>Origami is not available to residents of your country at this time.</p>
+  </FullScreenContainer>
+);
+
+const Main = () => {
+  // Check geoblock API
+  const [geoblock, setGeoblock] = useState<boolean>();
+  useEffect(() => {
+    const checkGeoblock = async () => {
+      const res = await fetch(GEOBLOCK_URL);
+      const data = await res.json();
+      if (data.blocked) setGeoblock(true);
+    };
+    checkGeoblock();
+  }, []);
+
+  return (
+    // Put the ApiManagerProvider outside StrictMode, so that the api only gets
+    // created once in dev.
+    <Web3OnboardProvider web3Onboard={WEB3_ONBOARD}>
+      <ApiManagerProvider apiConfig={API_CONFIG}>
+        <React.StrictMode>
+          <ThemeContext.Provider value={theme}>
+            <GlobalStyle />
+            {geoblock ? (
+              <AccessRestricted />
+            ) : (
+              <RouterProvider router={router} />
+            )}
+          </ThemeContext.Provider>
+        </React.StrictMode>
+      </ApiManagerProvider>
+    </Web3OnboardProvider>
+  );
+};
+
+const FullScreenContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  text-align: center;
+  overflow: hidden;
+`;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // Put the ApiManagerProvider outside StrictMode, so that the api only gets
-  // created once in dev.
-  <Web3OnboardProvider web3Onboard={WEB3_ONBOARD}>
-    <ApiManagerProvider apiConfig={API_CONFIG}>
-      <React.StrictMode>
-        <ThemeContext.Provider value={theme}>
-          <GlobalStyle />
-            <RouterProvider router={router} />
-        </ThemeContext.Provider>
-      </React.StrictMode>
-    </ApiManagerProvider>
-  </Web3OnboardProvider>
+  <Main />
 );
--- apps/dapp/src/urls.ts
+++ apps/dapp/src/urls.ts
@@ -5,6 +5,7 @@ const GMX_URL = 'https://gmx.io';
 const DISCLAIMER_URL = `${window.location.origin}${AppRoutes.Disclaimer}`;
 const TERMS_OF_SERVICE_URL = `${window.location.origin}${AppRoutes.TermsOfService}`;
 const PRIVACY_POLICY_URL = `${window.location.origin}${AppRoutes.PrivacyPolicy}`;
+const GEOBLOCK_URL = `${window.location.origin}/api/geoblock`;
 
 export {
   ORIGAMI_URL,
@@ -12,4 +13,5 @@ export {
   DISCLAIMER_URL,
   TERMS_OF_SERVICE_URL,
   PRIVACY_POLICY_URL,
+  GEOBLOCK_URL,
 };
